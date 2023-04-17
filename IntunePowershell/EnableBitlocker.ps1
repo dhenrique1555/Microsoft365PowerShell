e77d7fa231f9917933627f8861c6745607360ee5
dhenrique1555 <85200389+dhenrique1555@users.noreply.github.com>
2023-04-17 13:26:37 -0300

    Create EnableBitlocker.ps1

---
+++ IntunePowershell/EnableBitlocker.ps1
@@ -0,0 +1,25 @@
+start-transcript
+$debugpreference = "continue"
+
+#Disable Bitlocker first if needed
+Disable-Bitlocker -MountPoint C: 
+
+
+
+##Enable Bitlocker
+#Test What Would Happen if Bitlocker was enabled
+$BVol = Get-BitLockerVolume -MountPoint "C"
+$Bvol.KeyProtector
+Enable-Bitlocker -mountpoint "C:" -encryptionmethod XtsAes256 -recoverypasswordprotector  -whatif
+
+
+#Actually Enable bitlocker if no problem was detected before
+Enable-Bitlocker -mountpoint "C:" -encryptionmethod XtsAes256 -recoverypasswordprotector
+
+#Backup Recovery Keys to AZure AD
+$BLV = Get-BitLockerVolume -MountPoint "C:"
+BackupToAAD-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $BLV.KeyProtector[0].KeyProtectorId
+BackupToAAD-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $BLV.KeyProtector[1].KeyProtectorId
+BackupToAAD-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $BLV.KeyProtector[2].KeyProtectorId
+
+Stop-transcript
