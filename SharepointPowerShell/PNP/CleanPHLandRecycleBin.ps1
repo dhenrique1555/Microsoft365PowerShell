76644b2bba630f52b7c34e9554192af1ad4afbc1
dhenrique1555 <85200389+dhenrique1555@users.noreply.github.com>
2025-07-08 10:23:02 -0300

    Create CleanPHLandRecycleBin.ps1

---
+++ SharepointPowerShell/PNP/CleanPHLandRecycleBin.ps1
@@ -0,0 +1,13 @@
+$SiteURL = "https://kscsglobal.sharepoint.com/sites/LEX-TAS-1-OSP-DE"
+$ListName = "Permanentes Dokumentarchiv"
+  
+#Connect to PnP Online
+Connect-PnPOnline -Url $SiteURL -Interactive
+ 
+#Delete all files from the library
+Get-PnPList -Identity $ListName | Get-PnPListItem -PageSize 100 -ScriptBlock {
+    Param($items) Invoke-PnPQuery } | ForEach-Object { $_.Recycle() | Out-Null
+}
+
+$deleteitems = Get-PnPRecycleBinItem | where-object {$_.deletedbyemail -eq "adm_danilo.pinheiro@kscsglobal.onmicrosoft.com"}
+$deleteitems | Clear-PnpRecycleBinItem -force
