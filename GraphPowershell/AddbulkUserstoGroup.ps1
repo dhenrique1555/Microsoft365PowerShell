73751ba1d491f1c29251ab02bce736492f394811
dhenrique1555 <85200389+dhenrique1555@users.noreply.github.com>
2026-06-12 13:55:51 -0300

    Update AddbulkUserstoGroup.ps1

--- GraphPowershell/AddbulkUserstoGroup.ps1
+++ GraphPowershell/AddbulkUserstoGroup.ps1
@@ -10,3 +10,46 @@ $params = @{
 }
 
 New-MgGroupMemberByRef -GroupId e2121bd1-82ef-407e-8bf3-bcfd0144780b -BodyParameter $params
+
+#######################Version2
+Import-Module Microsoft.Graph.Groups
+
+# 1. Define the names of your Source and Target groups
+$SourceGroupName = ""
+$TargetGroupName = ""
+
+# 2. Find the Source Group and its ID
+$SourceGroup = Get-MgGroup -Filter "displayName eq '$SourceGroupName'"
+if (-not $SourceGroup) {
+    Write-Error "Could not find a source group named '$SourceGroupName'."
+    exit
+}
+
+# 3. Find the Target Group and its ID
+$TargetGroup = Get-MgGroup -Filter "displayName eq '$TargetGroupName'"
+if (-not $TargetGroup) {
+    Write-Error "Could not find a target group named '$TargetGroupName'."
+    exit
+}
+
+# 4. Grab all members from the Source Group
+# Note: The -All switch is critical here to bypass the default 100-item limit
+$SourceMembers = Get-MgGroupMember -GroupId $SourceGroup.Id -All
+
+# 5. Loop through the source members and add them to the Target Group
+foreach ($Member in $SourceMembers) {
+    $MemberId = $Member.Id
+
+    $params = @{
+        "@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$MemberId"
+    }
+
+    # try/catch block is used here because Graph will throw an error if the user is already in the group
+    try {
+        New-MgGroupMemberByRef -GroupId $TargetGroup.Id -BodyParameter $params
+        Write-Host "Successfully added member ID $MemberId to $TargetGroupName." -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Skipped member ID $MemberId. They might already be in the target group." -ForegroundColor Yellow
+    }
+}
